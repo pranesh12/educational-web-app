4d67940cdb712496cb9c3ea50f5fd6eaff8ca04c
Pranesh <praneshchakama6@gmail.com>
2021-07-25 15:10:34 +0600

    fake data created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch></Switch>
+          <Footer />
+        </Router>
+
+        <h1>Hello</h1>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>All reight reserverd</p>
+    </div>
+  );
+};
+
+export default Footer;
